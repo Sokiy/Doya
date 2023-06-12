ae2d56babecb70126e87d12e9ec97eac83f2b432
Sokiy <kangshao2015@gmail.com>
2023-06-12 19:32:20 +0800

    [feat]add drop feature

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,55 @@
 use fltk::{app, enums::Event, image::SharedImage, prelude::*, *};
 use std::time::{Duration, Instant};
+
+#[derive(Debug)]
+enum Msg {
+    Resize,
+    Parse,
+}
+
+fn main() {
+    let app = app::App::default().with_scheme(app::Scheme::Gtk);
+    let mut wind = create_window();
+
+    // Create a shared image
+    let path = "assets/noob.png";
+    let mut img = load_image(path);
+
+    // declare send rec channel
+    let (send, rec) = app::channel();
+
+    // first time draw the window
+    draw_image(&mut wind, &mut img);
+    // monitor the window event
+    monitor_window_event(&mut wind, send);
+
+    wind.make_resizable(true);
+    wind.end();
+    wind.show();
+
+    // 在主程序中监听 wind 的事件触发 send 的消息
+    while app.wait() {
+        if let Some(msg) = rec.recv() {
+            match msg {
+                Msg::Resize => draw_image(&mut wind, &mut img),
+                // Msg::Parse => println!("{}", app::event_text()),
+                Msg::Parse => {
+                    // 在第一次触发的时候,我们需要判断文件是否存在
+                    let path = std::path::PathBuf::from(&app::event_text());
+                    println!("{}", path.display());
+                    if path.exists() {
+                        println!("{}", app::event_text());
+                        img = load_image(app::event_text().as_str());
+                        draw_image(&mut wind, &mut img);
+                    }
+                }
+            }
+        }
+    }
+
+    app.run().unwrap();
+}
+
 ///create a window
 fn create_window() -> window::Window {
     const WIDTH: i32 = 500;
@@ -13,6 +63,26 @@ fn create_window() -> window::Window {
 fn load_image(path: &str) -> image::SharedImage {
     println!("load image from {}", path);
     SharedImage::load(path).unwrap()
+}
+
+///draw image
+fn draw_image(wind: &mut window::DoubleWindow, img: &mut image::SharedImage) {
+    let mut img = img.clone();
+
+    wind.draw(move |f| {
+        let (wind_w, wind_h) = (f.w() as f64, f.h() as f64);
+        let (img_w, img_h) = (img.w() as f64, img.h() as f64);
+        let (pos_x, pos_y) = calc_pos(&wind_w, &wind_h, &img_w, &img_h);
+        img.scale(f.w(), f.h(), true, true);
+        img.draw(pos_x, pos_y, img.w(), img.h());
+
+        println!(
+            "wind_w:{}, wind_h:{}, img_w:{}, img_h:{},pos_x:{}, pos_y:{}",
+            wind_w, wind_h, img_w, img_h, pos_x, pos_y
+        );
+    });
+
+    wind.redraw();
 }
 
 /// calc img position
@@ -32,82 +102,52 @@ fn calc_pos(wind_w: &f64, wind_h: &f64, img_w: &f64, img_h: &f64) -> (i32, i32) 
         let compute_img_h = wind_w * img_h / img_w;
         pos_y = ((wind_h - compute_img_h).abs() / 2.0).round() as i32;
     }
-    // println!("window_w:{}, window_h:{}, img_w:{}, img_h:{},wind_ratio:{}, img_ratio:{}, pos_x:{}, pos_y:{}", wind_w, wind_h, img_w, img_h, wind_ratio, img_ratio, pos_x, pos_y);
-
     (pos_x, pos_y)
 }
 
 ///监听 wind 的各种事件
-fn monitor_window_event(wind: &mut window::DoubleWindow, send: app::Sender<bool>) {
+fn monitor_window_event(wind: &mut window::DoubleWindow, send: app::Sender<Msg>) {
     //获取上次重绘时间,在重绘之后更新重绘时间
     let mut last_resize_time = Instant::now();
     //设置每次重绘的阈值
-    let resize_duration = Duration::from_millis(100);
+    let resize_duration = Duration::from_millis(200);
+    //是否已经进入
+    let mut dnd = false;
+    //是否已经释放
+    let mut relased = false;
 
     wind.handle(move |_, event| match event {
         Event::Resize => {
             if last_resize_time.elapsed() > resize_duration {
                 last_resize_time = Instant::now();
-                send.send(true);
+                send.send(Msg::Resize);
             }
             true
         }
+        //文件进入窗体  返回 true 表示已经被处理，不然捕获不到后续的事件
+        Event::DndEnter => {
+            dnd = true;
+            true
+        }
+        //文件拖拽
+        Event::DndDrag => true,
+        //文件在窗体中释放
+        Event::DndRelease => {
+            relased = true;
+            true
+        }
+        //文件粘贴完成, 进行重绘操作
+        Event::Paste => {
+            if dnd && relased {
+                //发送粘贴操作信号
+                send.send(Msg::Parse);
+                dnd = false;
+                relased = false;
+            }
+            true
+        }
+        //文件被拖出窗体
+        Event::DndLeave => true,
         _ => false,
     });
-}
-
-//draw image
-fn draw_image(wind: &mut window::DoubleWindow, img: &mut image::SharedImage) {
-    let (wind_w, wind_h, img_w, img_h) = (
-        wind.w() as f64,
-        wind.h() as f64,
-        img.w() as f64,
-        img.h() as f64,
-    );
-    let (pos_x, pos_y) = calc_pos(&wind_w, &wind_h, &img_w, &img_h);
-
-    let mut img = img.clone();
-
-    wind.draw(move |f| {
-        img.scale(f.w(), f.h(), true, true);
-        img.draw(pos_x, pos_y, img.w(), img.h());
-        println!(
-            "wind_w:{}, wind_h:{}, img_w:{}, img_h:{},pos_x:{}, pos_y:{}",
-            wind_w, wind_h, img_w, img_h, pos_x, pos_y
-        );
-    });
-
-    wind.redraw();
-}
-
-// fn resize_callback(w: &mut window::Window, _x: i32, _y: i32, _w: i32, _h: i32) {
-//     println!("Window resized to ({}, {})", w.width(), w.height());
-// }
-
-fn main() {
-    let app = app::App::default();
-    let mut wind = create_window();
-    let path = "assets/noob.png";
-    let mut img = load_image(path);
-
-    let (send, rec) = app::channel();
-
-    draw_image(&mut wind, &mut img);
-
-    // wind.resize_callback(Box::new(resize_callback));
-    monitor_window_event(&mut wind, send);
-
-    wind.make_resizable(true);
-    wind.end();
-    wind.show();
-
-    // 在主程序中监听 wind 的事件触发 send 的消息
-    while app.wait() {
-        if let Some(msg) = rec.recv() {
-            println!("{}", msg);
-            draw_image(&mut wind, &mut img);
-        }
-    }
-
-    app.run().unwrap();
 }
